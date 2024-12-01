82d046b9108328b75557eab63e034b0fd6e0d1b4
Gr1zzlyBear7 <parkurist77@yandex.ru>
2024-12-01 13:27:53 +0300

    Сделал 9, долго думал как, но всё таки смог, это моё первое задание тип Чертёжник

---
+++ 6(ЧЕРЕПАХА)/9.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+tracer(0)
+screensize(3000, 3000)
+k = 20
+lt(90)
+
+for i in range(10):
+    arr = pos()
+    goto((arr[0] + 3 * k), (arr[1] + 6 * k))
+    dot(6, 'black')
+    arr = pos()
+    goto((arr[0] + 7 * k), (arr[1] + -2 * k))
+    arr = pos()
+    dot(6, 'black')
+    goto((arr[0] + -10 * k), (arr[1] + -4 * k))
+    dot(6, 'black')
+
+pu()
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * k, y * k)
+        dot(5, 'red')
+done()

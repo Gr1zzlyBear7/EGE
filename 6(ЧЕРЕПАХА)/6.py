b2e6a6ff780e947bac2a231eef9a547229e94aa1
Gr1zzlyBear7 <parkurist77@yandex.ru>
2024-12-01 13:03:26 +0300

    Сделал 6

---
+++ 6(ЧЕРЕПАХА)/6.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+tracer(0)
+screensize(3000, 3000)
+k = 20
+lt(90)
+fd(9 * k)
+rt(90)
+for i in range(2):
+    fd(3 * k)
+    rt(90)
+    fd(3 * k)
+    rt(270)
+for i in range(2):
+    fd(3 * k)
+    rt(90)
+fd(9 * k)
+pu()
+for x in range(-30, 30):
+    for y in range(-30, 30):
+        goto(x * k, y * k)
+        dot(5, 'red')
+done()

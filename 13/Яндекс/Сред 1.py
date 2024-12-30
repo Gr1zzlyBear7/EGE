3805302b53d396a92a6cb631889587adf7983588
Gr1zzlyBear7 <parkurist77@yandex.ru>
2024-12-30 08:34:41 +0300

    Сделал Среднюю 1(неверно)

--- 13/Яндекс/Сред 1.py
+++ 13/Яндекс/Сред 1.py
@@ -1,6 +1,6 @@
 from ipaddress import *
 
-for m in range(32, -1, -1):
+for m in range(31, -1, -1):
     net = ip_network(f'137.219.220.63/{m}', 0)
-    if '255.255.255.255' in str(net):
+    if str(net.broadcast_address) in str(net):
         print(m)

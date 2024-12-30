dc441aa6f28f8556ced2b99727d395017ac9d2eb
Gr1zzlyBear7 <parkurist77@yandex.ru>
2024-12-30 08:41:27 +0300

    Сделал Среднюю 8

---
+++ 13/Яндекс/Средняя 8.py
@@ -0,0 +1,8 @@
+from ipaddress import *
+
+for m in range(33):
+    net1 = ip_network(f'151.172.115.121/{m}', 0)
+    net2 = ip_network(f'151.172.115.156/{m}', 0)
+    if net1 != net2:
+        print(m)
+        break

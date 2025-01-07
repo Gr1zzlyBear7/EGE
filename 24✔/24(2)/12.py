a6e4ec9e05624bbc66e6830ac385de69f1418672
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-07 19:53:22 +0300

    второй урок 12 задание

---
+++ 24✔/24(2)/12.py
@@ -0,0 +1,11 @@
+s = open('12.txt').readline()
+
+k = ma = 1
+for i in range(len(s) - 1):
+    if s[i] <= s[i + 1]:
+        k += 1
+    else:
+        if k > ma:
+            ma = k
+        k = 1
+print(ma)

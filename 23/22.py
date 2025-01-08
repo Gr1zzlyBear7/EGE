07265f276702db2719ba7eb01141ea6629c1348a
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-08 09:58:37 +0300

    сделал 22 из 23 сложненько

--- 23/22.py
+++ 23/22.py
@@ -1,8 +1,11 @@
 def f(x, y, k):
     if x > y:
         return 0
-    if x == y:
-        return k == 1
-    return f(x + 1, y, k) + f(x + 2, y, k) + f(x * 2, y, k + 1)
+    if x == y and k == '*':
+        return 1
+    if k == '*':
+        return f(x + 1, y, '*') + f(x + 2, y, '*')
+    return f(x + 1, y, '+') + f(x + 2, y, '+') + f(x * 2, y, '*')
 
-print(f(2, 12, 0))
+
+print(f(2, 12, '+'))

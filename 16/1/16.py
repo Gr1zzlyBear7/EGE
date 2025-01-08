5ac33cbb9e3dd62c63e009ed7e361070df72e067
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-08 20:28:19 +0300

    cделал прототип 16

---
+++ 16/1/16.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+
+@lru_cache
+def f(n):
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return f(n / 2) + 1
+    return f(n - 1) + n
+
+
+for n in range(1, 1000):
+    if f(n) == 19:
+        print(n)
+        break

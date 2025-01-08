4e9ee415760ee5481cd82c988b12fe177c37229a
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-08 15:33:35 +0300

    сделал пятый прототип

---
+++ 5/4.py
@@ -0,0 +1,17 @@
+ans = []
+for n in range(1, 1000):
+    bin_n = bin(n)[2:]
+    bin_n += bin_n[-1]
+    if bin_n.count('1') % 2 == 0:
+        bin_n += '0'
+    else:
+        bin_n += '1'
+    if bin_n.count('1') % 2 == 0:
+        bin_n += '0'
+    else:
+        bin_n += '1'
+    r = int(bin_n, 2)
+    if r > 144:
+        ans.append(r)
+
+print(min(ans))

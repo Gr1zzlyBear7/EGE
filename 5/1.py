1dfcd261a40bd321c45283bf9308d60d5ec1858f
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-08 15:28:59 +0300

    сделал пятый прототип

--- 5/1.py
+++ 5/1.py
@@ -1,2 +1,10 @@
-for i in range(1, 23):
-    open(str(i) + '.py', 'a')
+ans = []
+for n in range(1, 1000):
+    bin_n = bin(n)[2:]
+    bin_n += str(bin_n.count('1') % 2)
+    bin_n += str(bin_n.count('1') % 2)
+    r = int(bin_n, 2)
+    if r > 80:
+        ans.append(r)
+
+print(min(ans))

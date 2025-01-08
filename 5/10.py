273a71d0e4c8e430fcd23862ed3956209ffb10ff
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-08 15:47:00 +0300

    сделал пятый прототип

---
+++ 5/10.py
@@ -0,0 +1,12 @@
+ans = []
+for n in range(1, 1000):
+    b = bin(n)[2:]
+    if b.count('1') % 2 == 0:
+        b = '10' + b[2:] + '0'
+    else:
+        b = '11' + b[2:] + '1'
+    r = int(b, 2)
+    if r < 35:
+        ans.append(n)
+
+print(max(ans))

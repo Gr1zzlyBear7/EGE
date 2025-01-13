2c911ef9265fe59169616ac9d21706901d630527
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-13 17:53:53 +0300

    да

---
+++ 24✔/24(2)/15.py
@@ -0,0 +1,11 @@
+s = open('15.txt').readline()
+ans = []
+k = 0
+for i in range(len(s) - 1):
+    if s[i] + s[i + 1] == 'BB' or s[i] + s[i + 1] == 'DD':
+        k += 2
+    else:
+        ans.append(k)
+        k = 2
+
+print(max(ans))

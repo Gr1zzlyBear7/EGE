bf73938900c00ac6b33b6ac3d56a933f020fa4f0
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-14 16:50:00 +0300

    Галочка

--- 13✔/Яндекс/Средняя 25.py
+++ 13✔/Яндекс/Средняя 25.py
@@ -1,7 +1,7 @@
 from ipaddress import *
 
 for m in range(33):
-    net = ip_network(f'111.81.27.208/{m}', 0)
-    if '111.81.27.192' in str(net):
+    net = ip_network(f'111.81.27✔.208/{m}', 0)
+    if '111.81.27✔.192' in str(net):
         print(net.netmask)
         break

660441a02e9f159dbb674aa3b0834b0210bc3bad
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-21 17:46:12 +0300

    сделал прототип 26 задания

--- 26/26(1)/7.py
+++ 26/26(1)/7.py
@@ -1,2 +1,14 @@
-for i in range(7, 20):
-    open(str(i), 'a')
+k = 100
+m = 150
+data = list(map(int, open('7')))
+data.sort(reverse=True)
+bigest_sales = data[:k]
+data = data[k:]
+sales = data[:m]
+data = data[m:]
+total_sale = 0
+for sale in bigest_sales:
+    total_sale += sale * 0.2
+for sale in sales:
+    total_sale += sale * 0.1
+print(data[0], int(total_sale))

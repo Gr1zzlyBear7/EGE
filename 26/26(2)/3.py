216f782d3e23a436a8b0fedec39222628f241922
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-21 15:22:24 +0300

    сделал ещё одну 26, сам! Какой же я лютый сигма ё-ма-ё ;)

--- 26/26(2)/3.py
+++ 26/26(2)/3.py
@@ -1,2 +1,19 @@
-for i in range(3, 11):
-    open(str(i), 'a')
+s = 600
+data = list(map(int, open('3')))
+data.sort(reverse=True)
+ans = [0] * 1000
+k = 0
+while True:
+    to_del = []
+    for i in range(len(data)):
+        if ans[k] + data[i] <= s:
+            ans[k] += data[i]
+            to_del.append(data[i])
+    while to_del:
+        data.remove(to_del[0])
+        to_del.pop(0)
+    k += 1
+    if k == 1000:
+        break
+ans = [x for x in ans if x != 0]
+print(len(ans), ans[-1])

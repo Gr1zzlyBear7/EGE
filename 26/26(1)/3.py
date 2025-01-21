5114f56736fd180f64818c3ba41050debf3531fb
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-21 12:21:55 +0300

    сделал задачу из 26 типа

--- 26/26(1)/3.py
+++ 26/26(1)/3.py
@@ -1,18 +1,16 @@
-size = 524288
-data = sorted(list(map(int, open('3.txt'))))
-condition = size // 2
-videos = list(filter(lambda x: x >= 101, data))
-k = 0
-for video in videos:
-    if size <= condition:
-        print(size, video)
-        for photo in data:
-            if size >= photo:
-                k += 1
-                size -= photo
-        break
-    else:
-        size -= video
-        k += 1
+data = list(map(int, open('3.txt')))
+mem = 524288
+half_mem = mem // 2
+data.sort()
+arr = []
+vid = list(filter(lambda x: x >= 101, data))
+pho = list(filter(lambda x: x <= 100, data))
+while sum(arr) <= half_mem:
+    arr.append(vid.pop(0))
+while sum(arr) + pho[0] <= mem:
+    arr.append(pho.pop(0))
+for i in range(len(pho)):
+    if sum(arr) - arr[-1] + pho[i] <= mem:
+        arr[-1], pho[i] = pho[i], arr[-1]
 
-print(size, k)
+print(mem - sum(arr), len(arr))

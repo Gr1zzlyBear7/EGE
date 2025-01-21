7cf63436bb5249838b840b5614f65197152463a8
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-21 15:01:43 +0300

    сделал задачу из 26 типа

--- 26/26(2)/1.py
+++ 26/26(2)/1.py
@@ -1,14 +1,21 @@
 s = 100000
-n = 1000
-
-data = sorted(list(map(int, open('1.txt'))))
-arr = []
-
-while s >= sum(arr) + data[0]:
-    arr.append(data.pop(0))
-
-for i in range(len(data)):
-    if sum(arr) - arr[-1] + data[i] <= s:
-        arr[-1], data[i] = data[i], arr[-1]
-
-print(len(arr), arr[-1])
+data = list(map(int, open('1.txt')))
+data.sort()
+ans = []
+k = 0
+while True:
+    if sum(ans) + data[-1] <= s:
+        ans.append(data.pop())
+    else:
+        new = data[::-1]
+        for elem in new:
+            if sum(ans) + elem <= s:
+                ans.append(elem)
+                data.remove(elem)
+        else:
+            break
+    if sum(ans) + data[0] <= s:
+        ans.append(data.pop(0))
+    else:
+        break
+print(len(ans), ans[-1])

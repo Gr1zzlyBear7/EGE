c52534791c7738cb83a722a25408e7f1caa4ee8b
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-22 11:11:28 +0300

    в общем ошибку в 12 я не могу найти, я просто без понятия где она, я переписал программу, перечитал условие и ничего тот же самый ответ

--- Вариант Яндекс 7 января(88 из 95)/12.py
+++ Вариант Яндекс 7 января(88 из 95)/12.py
@@ -1,13 +1,12 @@
-for i in range(4, 10000):
-    s = '8' + i * '4'
+for n in range(4, 10000):
+    s = '8' + '4' * n
     while '4444' in s or '844' in s or '84' in s:
         if '4444' in s:
             s = s.replace('4444', '884', 1)
         if '844' in s:
             s = s.replace('844', '488', 1)
         if '84' in s:
-            s = s.replace('84', '3343', 1)
+            s = s.replace('84', '3343')
     if len(s) >= 20:
-        print(i)
+        print(n)
         break
-

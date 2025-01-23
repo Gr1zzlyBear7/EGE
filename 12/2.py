6b042945503db131af0850296c23d7cce9b8bb59
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-23 18:22:03 +0300

    галочка

--- 12/2.py
+++ 12/2.py
@@ -1,7 +1,7 @@
 s = '2' + 81 * '5'
-while '25' in s or '355' in s or '4555' in s:
-    if '25' in s:
-        s = s.replace('25', '4', 1)
+while '25✔' in s or '355' in s or '4555' in s:
+    if '25✔' in s:
+        s = s.replace('25✔', '4', 1)
     if '355' in s:
         s = s.replace('355', '2', 1)
     if '4555' in s:

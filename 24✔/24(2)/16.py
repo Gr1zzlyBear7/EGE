2f69b9890425b7f521c47832aaf7e893dea60762
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-26 10:40:14 +0300

    сделал 24 тип

---
+++ 24✔/24(2)/16.py
@@ -0,0 +1,9 @@
+s = open('16.txt').readline()
+m = [0] * len(s)
+for i in range(1, len(s)):
+    if s[i - 1] + s[i] == 'PC':
+        m[i] = m[i - 2] + 2
+    if i >= 3:
+        if s[i - 3] + s[i - 2] + s[i - 1] + s[i] == 'CSGO':
+            m[i] = m[i - 4] + 4
+print(max(m))

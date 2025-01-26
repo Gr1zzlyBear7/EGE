eadce5f85f03d8f988dd9348b0756a105a9025fa
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-26 10:33:55 +0300

    сделал 24 тип

--- 24✔/24(2)/15.py
+++ 24✔/24(2)/15.py
@@ -1,10 +1,6 @@
 s = open('15.txt').readline()
-ans = []
-k = 0
-for i in range(len(s) - 1):
-    if s[i] + s[i + 1] == 'BB' or s[i] + s[i + 1] == 'DD':
-        k += 2
-    else:
-        ans.append(k)
-        k = 2
-print(max(ans))
+m = [0] * len(s)
+for i in range(1, len(s)):
+    if s[i] + s[i - 1] == 'BB' or s[i] + s[i - 1] == 'DD':
+        m[i] = m[i - 2] + 1
+print(max(m))

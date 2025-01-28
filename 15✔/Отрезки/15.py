2b16a584856395751e6a991f4f7852a2671b6a48
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-28 14:05:38 +0300

    15 задание отрезки

--- 15✔/Отрезки/15.py
+++ 15✔/Отрезки/15.py
@@ -1,8 +1,18 @@
-a = 0
-f_usl = 1
-for x in [k * 0.25 for k in range(-10000, 10000)]:
-    b = 10 <= x <= 23
-    c = 12 <= x <= 30
-    f = not b or a or not c
-    if f_usl != f:
-        print(x)
+# a = 0
+# f_usl = 1
+# for x in [k * 0.25 for k in range(-10000, 10000)]:
+#     b = 10 <= x <= 23
+#     c = 12 <= x <= 30
+#     f = not b or a or not c
+#     if f_usl != f:
+#         print(x)
+
+x = 30
+b = list(range(10, 22))
+a = list(range(5, 18))
+c = list(range(12, 31))
+print((x not in b) and (x not in a) or (x in c))
+x = 9
+print((x not in b) and (x not in a) or (x in c))
+x = 4
+print((x not in b) and (x not in a) or (x in c))

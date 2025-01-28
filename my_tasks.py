b6b4ec128e9b56214ee619cfad9e15c122d71dad
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-01-28 14:36:46 +0300

    усовершенствовал массив тасков

--- my_tasks.py
+++ my_tasks.py
@@ -1,1 +1,10 @@
 arr = [2, 5, 6, 8, 9, 12, 13, 14, 15, 16, 17, '19-21', 23, 24, 25, 26, 27]
+arr = list(map(str, arr))
+for i in range(len(arr)):
+    arr[i] = arr[i] + '.py'
+arr.append('27a.txt')
+arr.append('27b.txt')
+arr.append('26.txt')
+arr.append('17.txt')
+arr.append('9.txt')
+print(arr)

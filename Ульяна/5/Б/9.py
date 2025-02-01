9a77ac67fa423414769718cd6fad2a4ff4571149
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 10:59:44 +0300

    тип 5

---
+++ Ульяна/5/Б/9.py
@@ -0,0 +1,19 @@
+def tri(n):
+    new = ''
+    while n > 0:
+        new += str(n % 3)
+        n //= 3
+    return new[::-1]
+
+
+ans = []
+for n in range(1, 1000):
+    tri_n = tri(n)
+    if sum(map(int, tri_n)) % 3 == 0:
+        tri_n += '212'
+    else:
+        tri_n += tri(sum(map(int, tri_n)) * 2)
+    r = int(tri_n, 3)
+    if r > 490:
+        ans.append(r)
+print(min(ans))

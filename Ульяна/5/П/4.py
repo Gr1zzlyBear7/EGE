c85398b024056888f6408f4dadb6eeeb155a36bc
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 10:24:16 +0300

    тип 5

---
+++ Ульяна/5/П/4.py
@@ -0,0 +1,19 @@
+def five(n):
+    new = ''
+    while n > 0:
+        new += str(n % 5)
+        n //= 5
+    return new[::-1]
+
+
+ans = []
+for n in range(1, 1000):
+    five_n = five(n)
+    if n % 25 == 0:
+        five_n = five_n[-3:] + five_n
+    else:
+        five_n += five(n % 25)
+    r = int(five_n, 5)
+    if r > 10000:
+        ans.append(n)
+print(min(ans))

850da1ce1efd6125d71099f2c5bb410ea60cf390
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 10:17:23 +0300

    тип 5

--- Ульяна/5/П/1.py
+++ Ульяна/5/П/1.py
@@ -1,2 +1,13 @@
-for i in range(1, 8):
-    open(str(i) + '.py', 'a')
+ans = []
+for n in range(1, 1000):
+    bin_n = bin(n)[2:]
+    if n % 4 == 0:
+        bin_n += bin_n[-2:]
+    else:
+        bin_n += bin(n % 4)[2:]
+    if bin_n[-1] == '0':
+        bin_n = bin_n[:-1]
+    r = int(bin_n, 2)
+    if r > 213:
+        ans.append(r)
+print(min(ans))

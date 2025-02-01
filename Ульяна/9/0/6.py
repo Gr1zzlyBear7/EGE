fd0e9e2dd8d76f3a464586e8855cbc8b84c9c56b
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 14:41:27 +0300

    тип 9

---
+++ Ульяна/9/0/6.py
@@ -0,0 +1,9 @@
+data = [list(map(int, x.split())) for x in open('6')]
+k = 0
+for line in data:
+    c1 = [line.count(x) for x in set(line)]
+    if 3 in c1 and len(c1) == 4:
+        arr = sorted(line, key=line.count)
+        if sum(arr[:3]) ** 2 < sum(arr[3:]) ** 2:
+            k += 1
+print(k)

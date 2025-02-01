4f02ba3bab41be7a51bed07d82f60592d5216f46
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 09:09:09 +0300

    тип 9

--- Ульяна/9/1.py
+++ Ульяна/9/1.py
@@ -1,2 +1,11 @@
-for i in range(1, 7):
-    open(str(i), 'a')
+data = [list(map(int, x.split())) for x in open('1')]
+k = 1
+for line in data:
+    c1 = [line.count(x) for x in set(line)]
+    if len(c1) == 5 and 3 in c1:
+        evens = [x for x in line if x % 2 == 0]
+        odds = [x for x in line if x % 2 == 1]
+        if len(evens) > len(odds):
+            print(k)
+    k += 1
+

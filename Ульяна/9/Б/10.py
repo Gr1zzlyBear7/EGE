ba6c86de6e61515655b10214e9fc078468535701
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 09:48:48 +0300

    тип 9

---
+++ Ульяна/9/Б/10.py
@@ -0,0 +1,7 @@
+data = [list(map(int, x.split())) for x in open('10')]
+for line in data:
+    c1 = [line.count(x) for x in set(line)]
+    if c1.count(3) == 2 and len(c1) == 4:
+        if line.count(min(line)) == 1:
+            print(sum(line))
+            break

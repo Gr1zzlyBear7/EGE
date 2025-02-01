3da26ad375d331dff76d812e45fc8053a0a1e091
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 09:31:46 +0300

    тип 9

--- Ульяна/9/Б/2.py
+++ Ульяна/9/Б/2.py
@@ -1,2 +1,9 @@
-for i in range(2, 11):
-    open(str(i), 'a')
+data = [list(map(int, x.split())) for x in open('2')]
+k = 0
+for line in data:
+    arr = sorted(line)
+    maxi = arr.pop()
+    if sum(arr) > maxi:
+        if len(set(line)) == 3:
+            k += 1
+print(k)

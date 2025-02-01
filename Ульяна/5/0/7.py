0491f099901c649296cf7180939632216017445e
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-01 14:15:05 +0300

    тип 5

---
+++ Ульяна/5/0/7.py
@@ -0,0 +1,19 @@
+def tri(n):
+    new = ''
+    while n > 0:
+        new += str(n % 3)
+        n //= 3
+    return new[::-1]
+
+
+ans = []
+for n in range(1, 10000):
+    t = tri(n)
+    if n % 3 == 0:
+        t = '1' + t + '02'
+    else:
+        t += tri((n % 3) * 4)
+    r = int(t, 3)
+    if r < 199:
+        ans.append(n)
+print(max(ans))

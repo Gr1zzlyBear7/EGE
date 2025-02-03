3053ee064f26f05e1dbe4eaf559a6013582ecc53
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-03 21:04:03 +0300

    24 type

---
+++ 24✔/ПОЛЯКОВ/2527.py
@@ -0,0 +1,12 @@
+s = open('2527.txt').readline()
+nums = []
+sub = ''
+for i in range(len(s)):
+    if s[i].isdigit():
+        sub += s[i]
+    else:
+        if sub:
+            if all([True if int(x) % 2 == 1 else False for x in sub]):
+                nums.append(int(sub))
+        sub = ''
+print(max(nums))

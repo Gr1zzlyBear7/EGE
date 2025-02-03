6e7df22e3c82eea7aa7b83f7529e853b8c9ef093
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-03 21:08:17 +0300

    24 type

---
+++ 24✔/ПОЛЯКОВ/2529.py
@@ -0,0 +1,22 @@
+s = open('2529.txt').readline()
+sub = ''
+subs = []
+for i in range(1, len(s)):
+    if s[i - 1] < s[i]:
+        if sub:
+            sub += s[i]
+        else:
+            sub += s[i - 1] + s[i]
+    else:
+        if sub:
+            subs.append(sub)
+        sub = ''
+print((max(subs, key=len)))
+m = [1] * len(s)
+for i in range(1, len(s)):
+    if s[i - 1] < s[i]:
+        m[i] = m[i - 1] + 1
+        if m[i] == 3:
+            print(s[i-2:i + 1])
+            break
+print(max(m))

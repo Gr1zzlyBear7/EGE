b39ebf2cfe167964794ffc91f602c31f565e046c
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-03 21:01:29 +0300

    24 type

---
+++ 24✔/ПОЛЯКОВ/2526.py
@@ -0,0 +1,11 @@
+s = open('2526.txt').readline()
+nums = []
+sub = ''
+for i in range(len(s)):
+    if s[i].isdigit():
+        sub += s[i]
+    else:
+        if sub:
+            nums.append(int(sub))
+        sub = ''
+print(min([x for x in nums if x % 2 == 0]))

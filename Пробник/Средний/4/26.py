d63313162a5aa4942d60423e5b2e7ebd333df7c4
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-05 08:53:47 +0300

    моё решение на 1 балл, как сделать второе условие не шарю

--- Пробник/Средний/4/26.py
+++ Пробник/Средний/4/26.py
@@ -1,33 +1,31 @@
+from itertools import chain
+
 
 k = 47
 s = 1263519
 m = 51
 data = [list(map(int, x.split())) for x in open('26.txt')]
-my_types = {x[0]: [] for x in data}
+my_types = {x: [] for x in range(1, k + 1)}
 for t, v in data:
     my_types[t].append(v)
     my_types[t].sort()
-total = 0
+total = []
 for t in my_types:
     arr = my_types[t]
-    i = 0
-    while i < m:
-        total += 1
-        s -= arr[0]
-        arr.pop(0)
-        i += 1
-ma = 0
-const = []
-for t in my_types:
-    total_s = s
-    k = m
-    useless = [0]
-    arr = my_types[t]
-    if arr:
-        while sum(useless) + arr[0] <= total_s:
-            k += 1
-            useless.append(arr.pop(0))
-            if not arr:
-                break
-        const.append(k)
-print(total - m + max(const), max(const))
+    for i in range(m):
+        total.append(arr.pop(0))
+details = list(chain.from_iterable([x for x in my_types.values()]))
+details.sort()
+while sum(total) + details[0] <= s:
+    total.append(details.pop(0))
+print(len(total))
+# my_types = {x: [] for x in range(1, k + 1)}
+# for t, v in data:
+#     my_types[t].append(v)
+#     my_types[t].sort()
+# arr = list(my_types.values())
+# while len(details):
+#     if details[0] in arr:
+#         arr.remove(details.pop(0))
+#
+# print(details)

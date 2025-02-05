085d63dfcfdf9b9f773c6f52dc5fdd4a5dcc15b7
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-05 09:10:11 +0300

    17 type

--- Ульяна/17/П/1.py
+++ Ульяна/17/П/1.py
@@ -1,2 +1,12 @@
-for i in range(1, 9):
-    open(str(i), 'a')
+data = list(map(int, open('1')))
+ans = []
+mi = min(data)
+ma = max(data)
+for i in range(len(data) - 2):
+    tri = [*data[i:i + 3]]
+    c1 = [x for x in tri if x % 3 == mi % 3]
+    if len(c1) == 1:
+        c2 = [x for x in tri if x % 7 == ma % 7]
+        if len(c2) >= 2:
+            ans.append(sum(tri))
+print(len(ans), max(ans))

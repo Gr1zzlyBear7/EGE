15a890a4a6adc1223b2ca60c120197524256bfcd
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-02-09 11:34:38 +0300

    17 type

---
+++ Ульяна/17/0/8.py
@@ -0,0 +1,10 @@
+data = list(map(int, open('8')))
+ans = []
+ma = max([x for x in data if abs(x) % 100 == 25])
+for i in range(len(data) - 2):
+    tri = [*data[i:i + 3]]
+    c1 = [x for x in tri if len(str(abs(x))) == 4]
+    if len(c1) <= 2:
+        if sum(tri) <= ma:
+            ans.append(sum(tri))
+print(len(ans), max(ans))

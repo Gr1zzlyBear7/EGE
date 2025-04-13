0b5a45c5eb68c4a3de0ff5a02dd4dafc6c61cb60
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-04-13 10:19:40 +0300

    одна из самых сложных задач с курса

---
+++ 24✔/soul/17685.py
@@ -0,0 +1,12 @@
+from re import *
+
+s = open('17685.txt').readline()
+
+num = r'([1-9][0-9]*|0)'
+reg = rf'{num}([+*]{num})+'
+reg = rf'(?=({reg}))'
+arr = sorted([x.group(1) for x in finditer(reg, s)], key=lambda x: -len(x))
+for x in arr:
+    if eval(x) == 0:
+        print(len(x))
+        break

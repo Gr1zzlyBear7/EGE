0c7fd8fd8de2ca5918c266f24fb3fc3df32de0e4
Gr1zzlyBear7 <parkurist77@yandex.ru>
2025-06-03 20:19:27 +0300

    файлы

--- Вариант Яндекс 14 июня/12.py
+++ Вариант Яндекс 14 июня/12.py
@@ -4,6 +4,6 @@ while '5454' in s or '554' in s or '444' in s:
         s = s.replace('5454', '2', 1)
     else:
         s = s.replace('554', '45', 1)
-    s = s.replace('444', '54')
+    s = s.replace('444', '54', 1)
 
 print(s)
